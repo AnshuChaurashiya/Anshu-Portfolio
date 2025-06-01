a2c17cb6be4b2083cfe6111eff32f3c1fbada7bd
AnshuChaurashiya <chaurashiya0016@gmail.com>
2025-06-01 09:50:26 -0700

    first commit

--- src/assets/data.js
+++ src/assets/data.js
@@ -1,41 +1,58 @@
- const projects = [
+const projects = [
     {
-        id: 1,
-        title: "Portfolio Website",
-        description: "A modern portfolio website showcasing my work and skills, built with React and featuring smooth animations and responsive design.",
-        image: "https://res.cloudinary.com/dbqgnaqqa/image/upload/v1747584505/chat_images/l8whcsjbkot5a4hf2rse.jpg",
-        link: "/",
-        tools: "React, Tailwind, GSAP",
-
-        category: "design"
+      id: 1,
+      title: "Portfolio Website",
+      description:
+        "A modern portfolio website showcasing my work and skills, built with React and featuring smooth animations and responsive design.",
+      image:
+        "https://res.cloudinary.com/dbqgnaqqa/image/upload/v1747584505/chat_images/l8whcsjbkot5a4hf2rse.jpg",
+      link: "/",
+      tools: "React, Tailwind, GSAP",
+      category: "design",
     },
     {
-        id: 2,
-        title: "EasyCart",
-        description: "Developed a fully functional eCommerce web application with features like product browsing, user authentication, shopping cart, and secure payment integration.",
-        image: "https://res.cloudinary.com/dbqgnaqqa/image/upload/v1747584190/chat_images/zr8ckyhhfvpbuampunfs.jpg",
-        link: "https://easycart2.netlify.app/",
-        category: "development",
-        tools: "React, Tailwind, Redux, Context, Firebase",
+      id: 2,
+      title: "EasyCart",
+      description:
+        "Developed a fully functional eCommerce web application with features like product browsing, user authentication, shopping cart, and secure payment integration.",
+      image:
+        "https://res.cloudinary.com/dbqgnaqqa/image/upload/v1747584190/chat_images/zr8ckyhhfvpbuampunfs.jpg",
+      link: "https://easycart2.netlify.app/",
+      category: "development",
+      tools: "React, Tailwind, Redux, Context, Firebase",
     },
     {
-        id: 3,
-        title: "ChatTime",
-        description: "A real-time chat application with features like user authentication, message history, online status, and group chat capabilities. Built with React and Socket.io.",
-        image: "https://res.cloudinary.com/dbqgnaqqa/image/upload/v1747586759/chat_images/yu6pggmnadtnoxxo2jjz.jpg",
-        link: "https://chat-web-app-6qam.onrender.com",
-        category: "development",
-        tools: "React, Socket.io, Node.js",
-
+      id: 3,
+      title: "ChatTime",
+      description:
+        "A real-time chat application with features like user authentication, message history, online status, and group chat capabilities. Built with React and Socket.io.",
+      image:
+        "https://res.cloudinary.com/dbqgnaqqa/image/upload/v1747586759/chat_images/yu6pggmnadtnoxxo2jjz.jpg",
+      link: "https://chat-web-app-6qam.onrender.com",
+      category: "development",
+      tools: "React, Socket.io, Node.js",
     },
     {
-        id: 4,
-        title: "Weather App",
-        description: "A weather forecasting application that provides real-time weather data, forecasts, and location-based weather information.",
-        image: "https://res.cloudinary.com/dbqgnaqqa/image/upload/v1747584505/chat_images/l8whcsjbkot5a4hf2rse.jpg",
-        link: "https://weatherapp.com",
-        category: "development"
-    }
-]
-
-export default projects;
+      id: 4,
+      title: "Apple vision",
+      description:
+        "This is Apple vision clone and it is fully responsive and built with React, Tailwind CSS, and GSAP.",
+      image:
+        "https://res.cloudinary.com/dbqgnaqqa/image/upload/v1748795725/chat_images/wscr3rnqkpng3hfcuuyi.jpg",
+      link: "https://verdant-toffee-45ed6b.netlify.app/",
+      category: "design",
+      tools: "React, Tailwind, GSAP",
+    },
+    {
+      id: 5,
+      title: "Artist Art",
+      description: "This design is a new UI for artists to present their artworks.",
+      image:
+        "https://res.cloudinary.com/dbqgnaqqa/image/upload/v1748795716/chat_images/bhpervugnjgzidsticnl.jpg",
+      link: "https://glittering-pithivier-f90e80.netlify.app/",
+      category: "design",
+      tools: "React, Tailwind, GSAP",
+    },
+  ];
+  
+  export default projects;
